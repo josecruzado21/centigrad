d1a816d401ea5bb4409abb56d21a9c1c95d0f091
Jose Cruzado <josecruzado2103@gmail.com>
2024-08-15 14:39:43 -0500

    feat: add layer and network

--- centigrad/layer.py
+++ centigrad/layer.py
@@ -1,7 +1,40 @@
 from variable import Variable
 
-class Layer():
-    def __init__(self, neurons, activation = "sigmoid"):
-        self.neurons = neurons
-        self.activarion = activation
+class Layer:
+    def __init__(self, 
+                 input_dim, 
+                 n_neurons, 
+                 activation = "sigmoid",
+                initial_weights=None,
+                initial_biases=None):
+        self.input_dim = input_dim
+        self.n_neurons = n_neurons
+        self.activation = activation
+        # Initial Weights
+        if initial_weights is not None:
+            self.weights = initial_weights
+        else:
+            self.weights = np.random.normal(loc=0, scale=1, size=(input_dim,n_neurons))
+        self.weights = np.vectorize(Variable)(self.weights)
+        # Initial Biases
+        if initial_biases is not None:
+            self.biases = initial_biases
+        else:
+            self.biases = np.random.normal(loc=0, scale=1, size = (n_neurons, 1))
+        self.biases = np.vectorize(Variable)(self.biases)
+    def __repr__(self):
+        return f"Layer(neurons={self.n_neurons}, activation = {self.activation})"
+    def forward(self, input_data):
+        self.input_dim = input_data.shape[0]
+        self.output_dim = input_data.shape[1]
+        linear_combination = np.matmul(self.weights.T, input_data) + self.biases
+        iteration_indexes = np.ndindex(linear_combination.shape[0], 
+                                        linear_combination.shape[1])
+        for entry in iteration_indexes:
+            activation = getattr(linear_combination[entry], self.activation)
+            linear_combination[entry] = activation()
+        return linear_combination
+    def backward(self):
+        gradient_method = getattr(Variable, "grad")
+        return np.vectorize(gradient_method)(self.weights), np.vectorize(gradient_method)(self.biases)
 
---
+++ centigrad/network.py
@@ -0,0 +1,37 @@
+class NeuralNetwork:
+    def __init__(self, 
+                 input_dim, 
+                 dimension, 
+                 activations, 
+                 cost_function = "l2", 
+                 epochs=10,
+                 initial_weights = None,
+                 initial_biases = None):
+        if (len(dimension)>0) & (len(dimension)==len(activations)):
+            self.depth = len(dimension)
+            self.dimension = dimension
+            self.activations = activations
+            self.cost_function = cost_function
+            self.layers = []
+            for idx, val in enumerate(dimension):
+                if len(self.layers)==0:
+                    input_dim_layer = input_dim
+                else: input_dim_layer = dimension[idx-1]
+                self.layers.append(Layer(input_dim = input_dim_layer,
+                                        n_neurons = val, 
+                                         activation = activations[idx],
+                                        initial_weights = initial_weights[idx] if initial_weights is not None else None,
+                                        initial_biases = initial_biases[idx] if initial_biases is not None else None))
+        else:
+            raise("NeuralNetwork should be at least 1 deep long")
+    
+    
+    def cost(self,X, y):
+        prediction = self.forward(X)
+        if self.cost_function == "l2":
+            return (((prediction - y)**2).sum().value)/(y.shape[1])
+                      
+    def forward(self, input_data):
+        for layer in self.layers:
+            input_data = layer.forward(input_data)
+        return input_data     
